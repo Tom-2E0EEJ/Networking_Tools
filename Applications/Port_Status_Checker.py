7740ee0ba752b86d207c16291af1a954ec11b23b
Tom <43816168+Tom-2E0EEJ@users.noreply.github.com>
2019-04-29 14:14:24 +0100

    Create Port_Status_Checker.py

---
+++ Applications/Port_Status_Checker.py
@@ -0,0 +1,17 @@
+
+"""
+    Pinger, This is a application that pings a network node.
+    Copyright (C) 2019 Thomas Scott
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+    
+    If you need to contact me for any reason please Email me at 2e0eej@gmx.com
+"""
